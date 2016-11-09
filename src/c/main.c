423d4f020a2ee21b3bc52b0b3d3c9bfd79d6bc0a
Jordan Donaldson <jedonaldson1@users.noreply.github.com>
2016-11-09 11:38:59 -0600

    Added text layer for time
    Haven't added time yet

--- src/c/main.c
+++ src/c/main.c
@@ -2,17 +2,32 @@
 
 //Variable declarations
 static Window *s_main_window;
+static TextLayer *s_time_layer;
 
 //Manages construction of Window's sub-elements
 static void main_window_load(Window *window)
 {
-  //code
+  //Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  
+  //Create the TextLayer for the time with specific bounds
+  s_time_layer = text_layer_create(GRect(0, 52, bounds.size.w, 50));
+  //Improve the layout to be more like a watchface
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
+  //Add as a child to Window root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 //Manages deconstruction of Window's sub-elements
 static void main_window_unload(Window *window)
 {
-  //code
+  //Destroy time layer
+  text_layer_destroy(s_time_layer);
 }
 
 //Handles main construction of watchface
